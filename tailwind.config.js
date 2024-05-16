748a78eff050247828b2c480c434505fee4e3f12
nourelhoudabmz <nourelhouda.iot@gmail.com>
2024-05-16 15:42:01 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-blue': '#007bff',
+      },
+    },
+  },
+  plugins: [],
+}
